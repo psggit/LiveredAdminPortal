d4a89284398375f843183f0049ae1dadde311916
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2019-05-13 18:31:45 +0530

    Added icons

--- src/constants/nav-items.js
+++ src/constants/nav-items.js
@@ -1,9 +1,9 @@
 export const menuItems = [
-  { label: 'Overview', value: 'overview', icon: 'cloudIcon'},
-  { label: 'Excise Departments', value: 'excise-departments', icon: 'creditsIcon'},
-  { label: 'Rules', value: 'rules', icon: 'reportsIcon'},
-  { label: 'Delivery Operators', value: 'delivery-operators', icon: 'supportIcon'},
-  { label: 'OTTP Management', value: 'ottp-management', icon: 'accountIcon'},
-  { label: 'User Management', value: 'user-management', icon: 'accountIcon'},
-  { label: 'Reports', value: 'reports', icon: 'accountIcon'},
+  { label: 'Overview', value: 'overview', icon: 'overviewIcon' },
+  { label: 'Excise Departments', value: 'excise-departments', icon: 'exciseDeptIcon' },
+  { label: 'Rules', value: 'rules', icon: 'rulesIcon' },
+  { label: 'Delivery Operators', value: 'delivery-operators', icon: 'dsoIcon' },
+  { label: 'OTTP Management', value: 'ottp-management', icon: 'ottpIcon' },
+  { label: 'User Management', value: 'user-management', icon: 'customerIcon' },
+  { label: 'Reports', value: 'reports', icon: 'reportsIcon' },
 ]
